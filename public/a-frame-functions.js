a8dc9c64910a1560fe177f7e7d46a7aeac426067
Alec Reimel <alecreimel1@gmail.com>
2024-09-28 11:32:42 -0400

    Reworking to make a full Astro project - shuffling code around

---
+++ public/a-frame-functions.js
@@ -0,0 +1,35 @@
+
+	function updateTime() {
+		const now = new Date();
+		const hours = now.getHours().toString().padStart(2, '0');
+		const minutes = now.getMinutes().toString().padStart(2, '0');
+		const seconds = now.getSeconds().toString().padStart(2, '0');
+		const timeString = `${hours}:${minutes}:${seconds}`;
+		const clockText = document.getElementById('LiveClock').getAttribute('text');
+
+    
+		document.getElementById('LiveClock').setAttribute('value', timeString);
+		//document.getElementById('LiveClock').flushToDom(true);
+    console.log("==real clock tick==");
+		return false;
+	}
+
+document.addEventListener("DOMContentLoaded", function(e){
+  //runs code after DOM load
+  // Initial call
+	updateTime();
+	// Update time every second
+	setInterval(updateTime, 1000);
+});
+
+	
+/* ===== ==================== ===== */
+/* ===== Session Timer ===== */
+/* ===== ==================== ===== */
+  var timer = new Timer();
+  timer.start();
+
+  timer.addEventListener('secondsUpdated', function (e) {
+    console.log("==timer tick==");
+    document.getElementById('SessionTimer').setAttribute('value', timer.getTimeValues().toString());
+  });
