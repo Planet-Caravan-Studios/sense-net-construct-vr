09ed65a12b34d4ea4e900ebdd26da4d861f2e7d8
Alec Reimel <alecreimel1@gmail.com>
2024-10-17 10:19:22 -0400

    click event set up and working

--- public/scripts/a-frame-functions.js
+++ public/scripts/a-frame-functions.js
@@ -1,17 +1,17 @@
 
-	function updateTime() {
-		const now = new Date();
-		const hours = now.getHours().toString().padStart(2, '0');
-		const minutes = now.getMinutes().toString().padStart(2, '0');
-		const seconds = now.getSeconds().toString().padStart(2, '0');
-		const timeString = `${hours}:${minutes}:${seconds}`;
-		const clockText = document.getElementById('LiveClock').getAttribute('text');
+function updateTime() {
+	const now = new Date();
+	const hours = now.getHours().toString().padStart(2, '0');
+	const minutes = now.getMinutes().toString().padStart(2, '0');
+	const seconds = now.getSeconds().toString().padStart(2, '0');
+	const timeString = `${hours}:${minutes}:${seconds}`;
+	const clockText = document.getElementById('LiveClock').getAttribute('text');
 
-    console.log("==clock tick==");
-		document.getElementById('LiveClock').setAttribute('value', timeString);
-		//document.getElementById('LiveClock').flushToDom(true);
-		return false;
-	}
+	console.log("==clock tick==");
+	document.getElementById('LiveClock').setAttribute('value', timeString);
+	//document.getElementById('LiveClock').flushToDom(true);
+	return false;
+}
 
 document.addEventListener("DOMContentLoaded", function(){
   //runs code after DOM load
@@ -21,4 +21,19 @@ document.addEventListener("DOMContentLoaded", function(){
 	setInterval(updateTime, 1000);
 });
 
+document.addEventListener("DOMContentLoaded", function(){
+	function toggleColor(element){
+		if(element.getAttribute("data-active") == "true"){
+			element.setAttribute("data-active", "false");
+			element.setAttribute("color", "#FF0000");
+		}else{
+			element.setAttribute("data-active", "true");
+			element.setAttribute("color", "#00FF00");
+		}
+	}
 	
+	let element = document.querySelector('#ClickTestCube');
+	element.addEventListener('click', function() {
+		toggleColor(element);
+	});
+});
